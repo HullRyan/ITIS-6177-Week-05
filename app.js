8c92b2e54b1d6264451295a8ef129c8763868250
Ryan Hull <hull.ryanc@gmail.com>
2023-09-26 19:55:13 -0400

    test

--- app.js
+++ app.js
@@ -154,6 +154,13 @@ app.get("/customers/:grade", (req, res) => {
  *     responses:
  *       200:
  *         description: Success
+ *         content:
+ *           application/json:
+ *              schema:
+ *                 type: object
+ *                 properties:
+ * 					 id:
+ * 					   type: string
  *       500:
  *         description: Internal Server Error
  *       400:
